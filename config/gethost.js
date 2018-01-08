0677ec994b2ff8252738141c4c81246f683fb2f7
paygle <dodar@qq.com>
2018-01-08 13:52:14 +0800

    更新

--- config/gethost.js
+++ config/gethost.js
@@ -1,14 +1,10 @@
-// 服务器主机端口配置
-var n = require('os').networkInterfaces();
-
-var myIp = module.exports = function () {
-  var ip = [];
-  for(var k in n) {
-    var inter = n[k];
-    for(var j in inter)
+let n = require('os').networkInterfaces();
+let myIp = function () {
+  for(let k in n) {
+    let inter = n[k];
+    for(let j in inter)
       if(inter[j].family === 'IPv4' && !inter[j].internal)
         return inter[j].address;
   }
 };
-
 module.exports = { ip: myIp() };
