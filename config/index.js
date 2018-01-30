1cbaa25c784fcfb60579cb119600470d8d812194
paygle <dodar@qq.com>
2018-01-30 17:46:12 +0800

    add proxy

--- config/index.js
+++ config/index.js
@@ -5,11 +5,29 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '',
-    proxyTable: {},
+    //用于跨域访问时使用代理地址
+    proxyTable: {
+      '/api': {
+        target: 'http://192.168.20.52:8090/',//设置你调用的接口域名和端口号 别忘了加http
+        changeOrigin: true ,
+        pathRewrite: {
+          //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替
+          // 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+          '^/api': ''
+        },
+        onProxyReq(proxyReq, req, res) {
+          //防止代理将headers字段变成小写
+          proxyReq.setHeader('biz_apiId', req.headers['biz_apiid']);
+          proxyReq.setHeader('biz_appKey', req.headers['biz_appkey']);
+          proxyReq.setHeader('biz_appSecret', req.headers['biz_appsecret']);
+          proxyReq.setHeader('rpc_group', req.headers['rpc_group']);
+          proxyReq.setHeader('rpc_version', req.headers['rpc_version']);
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: require('../config/gethost').ip, // can be overwritten by process.env.HOST
